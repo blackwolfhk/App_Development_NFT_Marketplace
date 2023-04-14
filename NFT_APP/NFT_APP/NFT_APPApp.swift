07fc4cd1fa883a97685bcb2703232a2007ee06cb
kelvincheung000 <cheungkamkit@gmail.com>
2023-04-15 00:08:24 +0800

    Updated and fixed package versions

---
+++ NFT_APP/NFT_APP/NFT_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  NFT_APPApp.swift
+//  NFT_APP
+//
+//  Created by Cheung Kam Kit on 14/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct NFT_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
